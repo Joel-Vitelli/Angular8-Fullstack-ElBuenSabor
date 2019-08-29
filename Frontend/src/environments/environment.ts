407a2f2c6f88e7364f757698ae048f3f5c51993d
Joel-Vitelli <jvitelli92@gmail.com>
2019-08-28 23:12:01 -0300

    Update Enviroments

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAqK8VWKRRocs2P7Sz41gakOSi301b4JNU",
-    authDomain: "elbuensabor-ccf43.firebaseapp.com",
-    databaseURL: "https://elbuensabor-ccf43.firebaseio.com",
-    projectId: "elbuensabor-ccf43",
+    apiKey: "<<>>",
+    authDomain: "<<>>",
+    databaseURL: "<<>>",
+    projectId: "<<>>",
     storageBucket: "",
-    messagingSenderId: "956754376236",
-    appId: "1:956754376236:web:92449b498797c022"
+    messagingSenderId: "<<>>",
+    appId: "<<>>"
   }
 };
 
